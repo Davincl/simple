f7775172825cb45a1587845312c68a5c5d415f72
Davincl <jong1318@gmail.com>
2021-03-31 15:15:24 +0900

    학습 과정 재 정의

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # simple
 spring boot simple frame rest API
+---
++ 개발 환경
+	- Spring Boot
+	- RestRepository & HAL Browser
+	- JPA
+	- IntelliJ IDE
+	- Maven
+	- postgreSQL
+
++ 학습 진행 과정
+	1. 개발 환경 준비
+	2. Spring initializr 프로젝트 생성
+	3. 간단한 게시판 DB 생성
--- src/main/java/com/simple/frame/common/filter/CorsFilter.java
+++ src/main/java/com/simple/frame/common/filter/CorsFilter.java
@@ -17,12 +17,16 @@ public class CorsFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        /*
+
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "*");
         chain.doFilter(request, servletResponse);
+
+         */
 
     }
 
